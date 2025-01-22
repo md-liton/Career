fe00acc8cd3fc5143a9e65c7ecc56bd080119fec
md-liton <mdliton5506@gmail.com>
2025-01-22 14:56:57 +0600

    first-file

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequatur, eaque deserunt tempore repellendus possimus nobis, adipisci molestiae est quam dolorum ratione nam optio fugit earum provident saepe, accusamus quia asperiores corporis quae quis. Ut, eius blanditiis, voluptatum explicabo laboriosam incidunt expedita quas maiores minima velit quidem aut ullam? Possimus sapiente distinctio quae deleniti nam sequi fuga autem facere nihil eaque laborum quam assumenda impedit sunt dolores veritatis fugit architecto, eveniet aspernatur nostrum tempore similique at recusandae. Iste impedit autem sequi quam enim, facere nobis numquam rerum adipisci, ex recusandae nisi! Odit quae qui eligendi delectus maiores. Harum, numquam a.</p>
+    </>
+  )
+}
+
+export default Home
