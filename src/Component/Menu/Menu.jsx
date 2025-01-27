d7a2f2a040f99a6a69d91b8d5a2d542dbd57f9dd
md-liton <mdliton5506@gmail.com>
2025-01-27 16:00:32 +0600

    Dropdown

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Menu = () => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [isSubDropdownVisible, setSubDropdownVisible] = useState(false);
   return (
     <div>
-        <ul className='flex justify-center items-center gap-[25px]'>
-            <li className='cursor-pointer'>Home</li>
-            <li className='cursor-pointer'>jobs</li>
-            <li className='cursor-pointer'>Apply</li>
-            <li className='cursor-pointer'>Notice</li>
-            <li className='cursor-pointer'>Contact</li>
-        </ul>
+      <ul className='flex justify-center items-center gap-[35px]'>
+        <li className='cursor-pointer'>Home</li>
+        <li className='cursor-pointer relative' onMouseEnter={() => setDropdownVisible(true)} onMouseLeave={() => setDropdownVisible(false)}>
+          <div>jobs</div>
+          {isDropdownVisible && (
+            <div className="job_item absolute text-white text-[14px] bg-[#F5F5F3] top-[20px] left-[-50px]  duration-400 z-[99] flex justify-between text-center ">
+              <div className=" mx-auto text-primary">
+                <ul className=" w-[150px]">
+                  <li className="py-[5px] hover:bg-red-500 hover:text-white duration-300 ">
+                    All Jobs
+                  </li>
+                  <li className="py-[5px] hover:bg-red-500 hover:text-white duration-300 ">
+                    Govment
+                  </li>
+                  <li className="py-[5px] hover:bg-red-500 hover:text-white duration-300 relative" onMouseEnter={() => setSubDropdownVisible(true)} onMouseLeave={() => setSubDropdownVisible(false)}>
+                    <div>Private</div>
+                    {isSubDropdownVisible && (
+                      <div className=" absolute text-white text-[14px] bg-[#F5F5F3] top-0 left-[150px]  duration-400 z-[99] flex justify-between text-center ">
+                        <div className=" mx-auto text-primary">
+                          <ul className=" w-[150px]">
+                            <li className="py-[5px] hover:bg-red-500 hover:text-white duration-300 ">
+                              Full Time
+                            </li>
+                            <li className="py-[5px] hover:bg-red-500 hover:text-white duration-300 ">
+                              Part Time
+                            </li>
+                            <li className="py-[5px] hover:bg-red-500 hover:text-white duration-300 ">
+                              Remote
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    )}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </li>
+        <li className='cursor-pointer'>Apply</li>
+        <li className='cursor-pointer'>Notice</li>
+        <li className='cursor-pointer'>Contact</li>
+      </ul>
     </div>
   )
 }
