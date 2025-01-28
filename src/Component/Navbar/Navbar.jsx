1641f751404020dfced2908141cba91c273da9ac
md-liton <mdliton5506@gmail.com>
2025-01-28 21:16:52 +0600

    Navbar-validation-small-device

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Menu from '../Menu/Menu'
-import { IoMenu ,IoClose} from "react-icons/io5";
+import { IoMenu, IoClose } from "react-icons/io5";
 
 
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false)
   return (
-    <nav className='bg-yellow-500'>
+    <nav className='bg-yellow-500 '>
       <div className="container">
-        <div className={`nav_main flex justify-between items-center py-[10px] bg-green-500`}>
+        <div className={`nav_main flex justify-between items-center py-[10px] bg-green-500 h-[50px] md:h-full`}>
           <div className={`logo h-[100ox] w-[100px] `}>Logo</div>
           <div className="menu">
             <div className='hidden md:block'>
@@ -16,24 +17,37 @@ const Navbar = () => {
             </div>
           </div>
           <div className='hidden md:block'>
-          <div className=" flex justify-end items-center gap-[15px] ">
-            <div className='w-[40px] h-[40px] rounded-full bg-red-500'></div>
-            <div className='bg-red-500 py-[8px] px-[25px] rounded-lg text-white cursor-pointer'>Login</div>
+            <div className=" flex justify-end items-center gap-[15px] ">
+              <div className='w-[40px] h-[40px] rounded-full bg-red-500'></div>
+              <div className='bg-red-500 py-[8px] px-[25px] rounded-lg text-white cursor-pointer'>Login</div>
+            </div>
           </div>
+
+          <div className="md:hidden">
+            {/* Toggle Menu Button */}
+            <IoMenu
+              className={`text-[25px] cursor-pointer ${menu && 'hidden'}`}
+              onClick={() => setMenu(!menu)}
+            />
+            <IoClose
+              className={`text-[25px] cursor-pointer ${!menu && 'hidden'}`}
+              onClick={() => setMenu(!menu)}
+            />
           </div>
-          <div className='md:hidden'>
-          <IoMenu  className='text-[25px] cursor-pointer'/>
-          </div>
-          <div className='md:hidden absolute top-0 left-0 z-[99] w-[80%] bg-red-500 rounded-r-lg'>
-            <div className='p-5 '>
-              <div className='flex justify-between items-center'>
-              <div className='w-[50px] h-[50px] rounded-full bg-green-500 '></div>
-              <IoClose className='text-[25px] cursor-pointer'/>
-              </div>
-              <div className=''>
-              <p className='text-[20px] font-bold'>name</p>
-              <Menu/>
-              <button className='bg-green-500 py-[8px] px-[25px] rounded-lg text-white cursor-pointer'>Login</button>
+
+          {/* Dropdown Menu */}
+          <div
+            className={`md:hidden absolute top-0 left-0 z-[99] w-[70%] bg-red-500 rounded-r-lg duration-500 transition-all transform 
+  ${menu ? 'translate-x-0 opacity-100 visible' : '-translate-x-full opacity-0 invisible'}`}
+          >
+            <div className="p-5">
+              <div className="w-[50px] h-[50px] rounded-full bg-green-500"></div>
+              <div>
+                <p className="text-[20px] font-bold">name</p>
+                <Menu />
+                <button className="bg-green-500 py-[8px] px-[25px] rounded-lg text-white cursor-pointer">
+                  Login
+                </button>
               </div>
             </div>
           </div>
